b8ef826ea26c31a79ae30ac323d432206f0ba10d
DimaChekashov <foxdima99@gmail.com>
2022-11-22 03:17:37 +0300

    add layout

---
+++ src/app/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Dictionary from '../Components/Dictionary/Dictionary';
+import { Layout, Menu } from 'antd';
+import './App.scss';
+
+const { Header, Content, Footer } = Layout;
+
+const App: React.FC = () => {
+  return (
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={new Array(15).fill(null).map((_, index) => {
+            const key = index + 1;
+            return {
+              key,
+              label: `nav ${key}`,
+            };
+          })}
+        />
+      </Header>
+      <Content style={{ padding: '0 50px', margin: '40px 0 0' }}>
+        <div className="site-layout-content">
+          <Dictionary />
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+  );
+}
+
+export default App;
